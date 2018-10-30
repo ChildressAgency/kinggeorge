b649072eb2438b3c468f0765c88363a9e63bdcb4
childressdev <jcampbell@childressagency.com>
2018-10-30 17:41:12 -0400

    added custom pagination

--- index.php
+++ index.php
@@ -29,8 +29,21 @@
                 echo '<p>Sorry, we could not find what you were looking for.</p>';
               }
             }
-            wp_pagenavi();
+            //wp_pagenavi();
           ?>
+          <div class="pio-full-list-pagination">
+            <?php
+              //https://codex.wordpress.org/Function_Reference/paginate_links
+              global $wp_query;
+              $big = 999999999;
+              echo paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                'format' => '?paged=%#%',
+                'current' => max(1, get_query_var('paged')),
+                'total' => $wp_query->max_num_pages
+              ));
+            ?>
+          </div>
         </div>
 
         <div class="col-sm-4 col-md-3">
