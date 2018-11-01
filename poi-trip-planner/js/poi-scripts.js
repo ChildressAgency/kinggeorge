73692d52e87aa7b8acad2fd7878a5cc2c47f6485
childressdev <jcampbell@childressagency.com>
2018-11-01 17:40:02 -0400

    map works for main map page

--- poi-trip-planner/js/poi-scripts.js
+++ poi-trip-planner/js/poi-scripts.js
@@ -1,29 +1,34 @@
 jQuery(document).ready(function($){
   //create map on page load
-  map = new_map('.poi-map');
+  if($('.poi-map').length){
+    map = new_map($('.poi-map'));
+  }
 
   //when user clicks a dropdown, add markers to map
-  $('#poi-nav').on('show.bs.dropdown', function(){
-    var pois = {};
-    $(this).find('.open>.dropdown-menu>li').each(function(index){
+  $('#poi-nav').on('shown.bs.dropdown', function(){
+    //var pois = {'markers':[]};
+    var pois = [];
+    var $poiItems = $(this).find('.open>.dropdown-menu>li');
+    
+    $poiItems.each(function(index){
       //https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
   
       var markers = [];
       markers['title'] = $(this).find('a').data('poi_title');
       markers['description'] = $(this).find('a').data('poi_description');
       markers['website'] = $(this).find('a').data('poi_website');
-      markers['lat'] = $(this).find('a').data('lat');
-      markers['lng'] = $(this).find('a').data('lng');
+      markers['lat'] = $(this).find('a').data('poi_lat');
+      markers['lng'] = $(this).find('a').data('poi_lng');
 
       pois.push(markers);
     });
 
-    add_markers(pois);
+    add_markers(pois, map);
   });
 
   //show the mytrip map
-  $('#mytrip-map').on('show.bs.collapse', function(){
-    var pois = {};
+  $('#mytrip-map').on('shown.bs.collapse', function(){
+    var pois = [];
     $('.mytrip-listing').each(function(index){
       var markers = [];
       markers['title'] = $(this).data('poi_title');
@@ -35,7 +40,8 @@ jQuery(document).ready(function($){
       pois.push(markers);
     });
 
-    add_markers(pois);
+    add_markers(pois, map);
+    google.maps.event.trigger(map, 'resize');
   });
 
   //set header counter and btns for my-trip
@@ -43,7 +49,7 @@ jQuery(document).ready(function($){
   update_my_trip_btns();
 
   //add to trip
-  $('.add-to-trip').on('click', function(e){
+  $('.add-remove-trip').on('click', '.add-to-trip', function(e){
     e.preventDefault();
     var poiId = $(this).data('poi_id');
     var poiIds = '';
@@ -63,10 +69,11 @@ jQuery(document).ready(function($){
 
     Cookies.set('poi_ids', poiIds, { expires:30 });
     update_my_trip_btns();
+    update_my_trip_counter();
   });
 
   //remove from trip
-  $('.remove-from-trip').on('click', function(e){
+  $('.add-remove-trip').on('click', '.remove-from-trip', function(e){
     e.preventDefault();
     var poiId = $(this).data('poi_id');
     var poiIds = '';
@@ -90,7 +97,14 @@ jQuery(document).ready(function($){
 
 function update_my_trip_counter(){
   var savedPois_cookie = Cookies.get('poi_ids');
-  $('#my-trip-icon').attr('data-count', savedPois_cookie.length);
+  savedPois_count = 0;
+
+  if(savedPois_cookie){
+    var savedPois = savedPois_cookie.split(',').map(Number);
+
+    savedPois_count = savedPois.length;
+  }
+  $('#my-trip-icon').attr('data-count', savedPois_count);
 }
 
 function update_my_trip_btns(){
@@ -108,15 +122,26 @@ function update_my_trip_btns(){
 }
 
 function new_map($el) {
-  //38.264493,-77.2198848
+  //38.267451,-77.180927
   var args = {
-    zoom: 16,
-    center: new google.maps.LatLng(38.264493, -77.2198848),
+    zoom: 14,
+    center: new google.maps.LatLng('38.267451', '-77.180927'),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   // create map	        	
   var map = new google.maps.Map($el[0], args);
+
+  //map.markers = [];
+
+  //add default marker
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng('38.267451', '-77.180927'),
+    map: map
+  });
+  markers.push(marker);
+  setMapOnAll(map)
+  //end default marker
 
   // center map
   center_map(map);
@@ -125,30 +150,47 @@ function new_map($el) {
   return map;
 }
 
+function setMapOnAll(map){
+  for(var i = 0; i < markers.length; i++){
+    markers[i].setMap(map);
+  }
+}
+
 function add_markers(pois, map){
+  //console.log(pois);
+      if(markers){
+        setMapOnAll(null);
+      }
   $.each(pois, function(key, poi){
-    var latlng = new google.maps.LatLng(poi.lat, poi.lng);
-    var infoWindowContent = '';
-    
-    infoWindowContent += '<h4>' + poi.title + '</h4>';
-    infoWindowContent += '<p>' + poi.description + '</p>';
-    infoWindowContent += '<a href="' + poi.website + '">Visit Website</a>';
+    if(poi.lat !== '' && poi.lng !== ''){
+      var latlng = new google.maps.LatLng(poi.lat, poi.lng);
+      var infoWindowContent = '';
+      
+      infoWindowContent += '<h4>' + poi.title + '</h4>';
+      infoWindowContent += '<p>' + poi.description + '</p>';
+      infoWindowContent += '<a href="' + poi.website + '" target="_blank">Visit Website</a>';
 
+      var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+      });
 
-    var marker = new google.maps.Marker({
-      position: latlng,
-      map: map
-    });
+      markers.push(marker);
 
-    map.markers.push(marker);
+      
+      var infoWindow = new google.maps.InfoWindow({
+        content: infoWindowContent
+      });
 
-    var infoWindow = new google.maps.InfoWindow({
-      content: infoWindowContent
-    });
+      google.maps.event.addListener(marker, 'click', function(){
+        if(prevInfoWindow){
+          prevInfoWindow.close();
+        }
 
-    google.maps.event.addListener(marker, 'click', function(){
-      infoWindow.open(map, marker);
-    });
+        prevInfoWindow = infoWindow;
+        infoWindow.open(map, marker);
+      });
+    }
   });
 
   center_map(map);
@@ -157,18 +199,17 @@ function add_markers(pois, map){
 function center_map(map) {
   // vars
   var bounds = new google.maps.LatLngBounds();
-
   // loop through all markers and create bounds
-  $.each(map.markers, function (i, marker) {
+  $.each(markers, function (i, marker) {
     var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
     bounds.extend(latlng);
   });
 
   // only 1 marker?
-  if (map.markers.length == 1) {
+  if (markers.length == 1) {
     // set center of map
     map.setCenter(bounds.getCenter());
-    map.setZoom(16);
+    map.setZoom(14);
   }
   else {
     // fit to bounds
@@ -178,3 +219,5 @@ function center_map(map) {
 
 // global var
 var map = null;
+var markers = [];
+var prevInfoWindow = false;
