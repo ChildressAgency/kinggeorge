062609dd1c04c81dbe0710b8f423c5002aca3eff
childressdev <jcampbell@childressagency.com>
2018-11-09 09:59:05 -0500

    recoded poi meta

--- poi-trip-planner/poi-full-list.php
+++ poi-trip-planner/poi-full-list.php
@@ -5,27 +5,45 @@
       'post_type' => 'poi',
       'posts_per_page' => 14,
       'post_status' => 'publish',
-      'paged' => $paged
+      'paged' => $paged,
+      'orderby' => 'post_title',
+      'order' => 'ASC'
     ));
 
     if($pois->have_posts()): while($pois->have_posts()): $pois->the_post(); ?>
       <div class="attractions-list-item" role="list-item">
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-        <p><?php the_field('street_address'); ?><br /><?php the_field('city_state_zip'); ?></p>
-        <p><?php the_field('phone'); ?></p>
         <?php 
-          $website = get_field('website');
-          $website_title = $website['title'];
-          if($website_title !== ''){
-            $website_title = $website['url'];
+          $street_address = get_field('street_address');
+          $city_state_zip = get_field('city_state_zip');
+
+          if($street_address || $city_state_zip){
+            echo '<p>';
+              echo $street_address ? $street_address : '';
+              if($street_address && $city_state_zip){ echo '<br />'; }
+              echo $city_state_zip ? $city_state_zip : '';
+            echo '</p>';
           }
+
+          $phone = get_field('phone');
+          echo $phone ? '<p>' . $phone . '</p>' : '';
+
+          $website = get_field('website');
+          if(!empty($website)){
+            $website_title = $website['title'];
+            if($website_title == ''){
+              $website_title = $website['url'];
+            }
+            echo '<p><a href="' . $website['url'] . '" target="_blank">' . $website_title . '</a></p>';
+          }
+
+          $map_description = get_field('map_description');
+          echo $map_description ? '<p>' . $map_description . '</p>' : '';
         ?>
-        <p><a href="<?php echo $website['url']; ?>" target="_blank"><?php echo $website_title; ?></a></p>
-        <p><?php the_field('map_description'); ?></p>
       </div>
   <?php endwhile; endif; ?>
 </div>
-<div class="pio-full-list-pagination">
+<div class="poi-pagination">
   <?php
     //https://codex.wordpress.org/Function_Reference/paginate_links
     $big = 999999999;
@@ -33,7 +51,9 @@
       'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
       'format' => '?paged=%#%',
       'current' => max(1, get_query_var('paged')),
-      'total' => $pois->max_num_pages
+      'total' => $pois->max_num_pages,
+      'prev_text' => '<',
+      'next_text' => '>'
     ));
   ?>
 </div>
