66f28578b66e94adb0e3dfaef49966ea67995fa8
childressdev <jcampbell@childressagency.com>
2018-11-28 12:58:19 -0500

    changed fluid to regular container

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <main id="main">
-    <div class="container-fluid">
+    <div class="container">
       <div class="row">
         <div class="col-sm-8 col-md-9 right-border">
           <?php if(have_posts()): while(have_posts()): the_post(); ?>
