1531bcf2b60f43d5ca7940a1f231d8d73004dd80
childressdev <jcampbell@childressagency.com>
2018-11-28 12:58:01 -0500

    added slick for instagram feed

--- js/kinggeorge-scripts.js
+++ js/kinggeorge-scripts.js
@@ -13,4 +13,21 @@ jQuery(document).ready(function($){
   $('.share-link').on('focus', function(){
     $(this).select();
   });
+
+  if(typeof $.fn.slick == 'function'){
+    $('.instagram-feed').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      variableWidth: true,
+      //centerMode: true,
+      //centerPadding: '60px',
+      dots: false,
+      arrows: true,
+      nextArrow: $('.instagram_next'),
+      prevArrow: $('.instagram_prev'),
+      autoplay: true,
+      autoplaySpeed: 7000
+    });
+  }
 });
