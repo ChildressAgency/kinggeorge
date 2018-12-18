4ca48ab0fc84d208c1bf29153d22fe64d01cb099
childressdev <jcampbell@childressagency.com>
2018-12-17 21:57:19 -0500

    added esc

--- page-my-trip.php
+++ page-my-trip.php
@@ -6,19 +6,19 @@
         <div class="col-sm-4">
           <div class="mytrip-step">
             <span class="step-number"><span>1</span></span>
-            <p><?php the_field('step_one_text'); ?></p>
+            <p><?php echo wp_kses_post(get_field('step_one_text')); ?></p>
           </div>
         </div>
         <div class="col-sm-4">
           <div class="mytrip-step">
             <span class="step-number"><span>2</span></span>
-            <p><?php the_field('step_two_text'); ?></p>
+            <p><?php echo wp_kses_post(get_field('step_two_text')); ?></p>
           </div>
         </div>
         <div class="col-sm-4">
           <div class="mytrip-step">
             <span class="step-number"><span>3</span></span>
-            <p><?php the_field('step_three_text'); ?></p>
+            <p><?php echo wp_kses_post(get_field('step_three_text')); ?></p>
           </div>
         </div>
       </div>
