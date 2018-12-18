608021adbd08fa22390e48e6a4f061a6bbad12b4
childressdev <jcampbell@childressagency.com>
2018-12-17 21:56:52 -0500

    added esc

--- page.php
+++ page.php
@@ -11,7 +11,7 @@
         $page_header = get_field('page_header', get_the_ID());
       }
       if($page_header): ?>
-        <h2 class="page-header"><?php echo $page_header; ?></h2>
+        <h2 class="page-header"><?php esc_html_e($page_header); ?></h2>
     <?php endif; ?>
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
       <?php the_content(); ?>
